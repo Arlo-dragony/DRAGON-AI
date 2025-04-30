774f58da1b16baac5b806344fb4401cc2a84c7a7
🔸A͜͡R͜͡L͜͡O͜͡D͜͡R͜͡A͜͡G͜͡O͜͡N͜͡🔸 <arlodeadpool@gmail.com>
2025-04-30 07:59:01 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const autoviewstatus = process.env.AUTOVIEW_STATUS || 'TRUE';
 const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
 const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE';
 
-const prefix = process.env.PREFIX || '+';
+const prefix = process.env.PREFIX || '.';
 const appname = process.env.APP_NAME || '';
 const herokuapi = process.env.HEROKU_API;
 const gptdm = process.env.GPT_INBOX || 'FALSE';
@@ -21,7 +21,7 @@ const antibot = process.env.ANTIBOT || 'FALSE';
 const author = process.env.STICKER_AUTHOR ||'⚜️DRAGON-AI⚜️';
 const packname = process.env.STICKER_PACKNAME || 'DRAGON-AI🗿';
 const antitag = process.env.ANTITAG || 'TRUE';
-const dev = process.env.DEV || '255759900549';
+const dev = process.env.DEV || '254720660277';
 
 const menu = process.env.MENU_TYPE || 'VIDEO';
 const DevRaven = dev.split(",");
@@ -35,9 +35,9 @@ const group = process.env.GROUP_ONLY_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺�
 const botAdmin = process.env.BOT_ADMIN_MSG || '𝗜 𝗻𝗲𝗲𝗱 𝗔𝗱𝗺𝗶𝗻 𝗽𝗿𝗲𝘃𝗶𝗹𝗲𝗱𝗴𝗲𝘀!';
 const NotOwner = process.env.NOT_OWNER_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝘁𝗵𝗲 𝗼𝘄𝗻𝗲𝗿!';
 
-const wapresence = process.env.WA_PRESENCE || 'typing';
+const wapresence = process.env.WA_PRESENCE || 'recording';
 const antilink = process.env.ANTILINK || 'FALSE';
-const mycode = process.env.CODE || '255';
+const mycode = process.env.CODE || '254';
 const antiforeign = process.env.ANTIFOREIGN || 'FALSE';
 const port = process.env.PORT || 4000;
 const antilinkall = process.env.ANTILINK_ALL || 'FALSE';
