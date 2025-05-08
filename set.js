03ce0c9af3c728ae42d72c730d7f3c0d541544e5
🔸A͜͡R͜͡L͜͡O͜͡D͜͡R͜͡A͜͡G͜͡O͜͡N͜͡🔸 <arlodeadpool@gmail.com>
2025-05-08 12:25:40 +0300

    Update set.js

--- set.js
+++ set.js
@@ -4,9 +4,9 @@ There's no need to configure this if you're deploying via Heroku — just set th
 
 const sessionName = 'session';
 const session = process.env.SESSION || '';
-const autobio = process.env.AUTOBIO || 'FALSE';
+const autobio = process.env.AUTOBIO || 'TRUE';
 const autoviewstatus = process.env.AUTOVIEW_STATUS || 'TRUE';
-const autolike = process.env.AUTOLIKE_STATUS || 'FALSE';
+const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
 const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE';
 
 const prefix = process.env.PREFIX || '.';
@@ -21,14 +21,14 @@ const antibot = process.env.ANTIBOT || 'FALSE';
 const author = process.env.STICKER_AUTHOR ||'⚜️DRAGON-AI⚜️';
 const packname = process.env.STICKER_PACKNAME || 'DRAGON-AI🗿';
 const antitag = process.env.ANTITAG || 'TRUE';
-const dev = process.env.DEV || '255710429490';
+const dev = process.env.DEV || '254787546219';
 
 const menu = process.env.MENU_TYPE || 'VIDEO';
 const DevRaven = dev.split(",");
 const badwordkick = process.env.BAD_WORD_KICK || 'FALSE';
 const bad = process.env.BAD_WORD || 'fuck,ku';
 const autoread = process.env.AUTOREAD || 'FALSE';
-const antidel = process.env.ANTIDELETE || 'FALSE';
+const antidel = process.env.ANTIDELETE || 'TRUE';
 
 const admin = process.env.ADMIN_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗿𝗲𝘀𝗲𝗿𝘃𝗲𝗱 𝗳𝗼𝗿 𝗔𝗱𝗺𝗶𝗻𝘀!';
 const group = process.env.GROUP_ONLY_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝗚𝗿𝗼𝘂𝗽𝘀!';
@@ -36,11 +36,11 @@ const botAdmin = process.env.BOT_ADMIN_MSG || '𝗜 𝗻𝗲𝗲𝗱 𝗔𝗱�
 const NotOwner = process.env.NOT_OWNER_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝘁𝗵𝗲 𝗼𝘄𝗻𝗲𝗿!';
 
 const wapresence = process.env.WA_PRESENCE || 'typing';
-const antilink = process.env.ANTILINK || 'FALSE';
+const antilink = process.env.ANTILINK || 'TRUE';
 const mycode = process.env.CODE || '254';
 const antiforeign = process.env.ANTIFOREIGN || 'FALSE';
 const port = process.env.PORT || 4000;
-const antilinkall = process.env.ANTILINK_ALL || 'FALSE';
+const antilinkall = process.env.ANTILINK_ALL || 'TRUE';
 
 module.exports = {
   session,
