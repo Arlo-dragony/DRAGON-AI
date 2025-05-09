8f652fa5c9ac4a2d9d29c5a0868224c6ec674627
🔸A͜͡R͜͡L͜͡O͜͡D͜͡R͜͡A͜͡G͜͡O͜͡N͜͡🔸 <arlodeadpool@gmail.com>
2025-05-09 17:01:16 +0300

    Update set.js

--- set.js
+++ set.js
@@ -21,7 +21,7 @@ const antibot = process.env.ANTIBOT || 'FALSE';
 const author = process.env.STICKER_AUTHOR ||'⚜️DRAGON-AI⚜️';
 const packname = process.env.STICKER_PACKNAME || 'DRAGON-AI🗿';
 const antitag = process.env.ANTITAG || 'TRUE';
-const dev = process.env.DEV || '254787546219';
+const dev = process.env.DEV || '256781579306';
 
 const menu = process.env.MENU_TYPE || 'VIDEO';
 const DevRaven = dev.split(",");
